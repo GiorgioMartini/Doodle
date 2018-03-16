5814d859a943529ae5755eb50e9ee7dbf28106a1
Giorgio <giorgio@giorgios-mbp.localdomain>
2018-03-16 22:27:37 +0100

    first commit, added webpack

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1 className="pa4">Babbel bowling</h1>
+          Hello {this.props.name}
+        </div>
+      );
+    }
+  }
+  
+ReactDOM.render(
+<HelloMessage name="Giorgio" />,
+document.getElementById('root')
+)
+
