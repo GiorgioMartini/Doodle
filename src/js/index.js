fa8186ae3a596d066de6fe820eedbbe8333c882b
Giorgio <giorgio@giorgios-mbp.localdomain>
2018-03-17 00:22:19 +0100

    started building the html

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 class HelloMessage extends React.Component {
     render() {
       return (
         <div>
-          <h1 className="pa4">
-          Babbel bowlingo
+          <h1 className="ma0 pa5 bg-near-black tc white">
+          Babbel bowling
           </h1>
-          <p>Hello {this.props.name}</p>
+          {/* <p>Hello {this.props.name}</p> */}
+          <div className="mw8 center pt4">
+            <div className="pa4">firld</div>
+            <div className="cf">
+              <div className="fl w-50">
+              <button>Shoot!</button>
+              </div>  
+              <div className="fl w-50">
+              <button>Shoot!</button>
+              </div>  
+            </div>
+          </div>
         </div>
-      );
+      )
     }
   }
   
@@ -18,4 +29,3 @@ ReactDOM.render(
 <HelloMessage name="Giorgio" />,
 document.getElementById('root')
 )
-
